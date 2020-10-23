688c00c4b9e8d2408ee1a59864bb1cb50065b5dc
Tauqeer Nasir <tauqeer.insta@gmail.com>
2020-10-23 16:47:19 +0400

    Updates

---
+++ precache-manifest.cfc1505396ec4c62d0edde805bc376f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfeb7f82bcf21da27792e5a75bc8edf4",
+    "url": "/tssg-syntax-editor/index.html"
+  },
+  {
+    "revision": "cde48eb4146a6f6fd450",
+    "url": "/tssg-syntax-editor/static/css/2.9df7aa81.chunk.css"
+  },
+  {
+    "revision": "7e8cb37a355b1c4c4d6f",
+    "url": "/tssg-syntax-editor/static/css/main.0c6f7cd8.chunk.css"
+  },
+  {
+    "revision": "cde48eb4146a6f6fd450",
+    "url": "/tssg-syntax-editor/static/js/2.798daf4a.chunk.js"
+  },
+  {
+    "revision": "8e78d2827888b2a1a1feb7eb48e0f6ce",
+    "url": "/tssg-syntax-editor/static/js/2.798daf4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8cb37a355b1c4c4d6f",
+    "url": "/tssg-syntax-editor/static/js/main.94fa5c64.chunk.js"
+  },
+  {
+    "revision": "55f4f8c13a53197328ac",
+    "url": "/tssg-syntax-editor/static/js/runtime-main.52010163.js"
+  }
+]);
